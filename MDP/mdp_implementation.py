75c0f7893b9e57661891c5afbaa103be6f2ece2a
lotanamit5 <lotan.amit@gmail.com>
2023-07-01 13:37:35 +0300

    mdp implementation DONE
    Co-authored-by: liorvardi <liorvardi@users.noreply.github.com>

--- MDP/mdp_implementation.py
+++ MDP/mdp_implementation.py
@@ -1,5 +1,54 @@
 from copy import deepcopy
 import numpy as np
+from mdp import MDP
+
+actions_to_idx = {"UP":0, "DOWN":1, "RIGHT":2, "LEFT":3}
+idx_to_actions = ["UP", "DOWN", "RIGHT", "LEFT"]
+
+@staticmethod
+def get_states(mdp: MDP):
+    for i in range(mdp.num_row):
+        for j in range(mdp.num_col):
+            if mdp.board[i][j] == "WALL":
+                continue
+            yield (i, j)
+
+@staticmethod
+def reward(mdp: MDP, state):
+    i, j = state
+    return float(mdp.board[i][j])
+
+@staticmethod
+def action_from_policy(policy, s_from):
+    i, j = s_from
+    return policy[i][j]
+
+@staticmethod
+def transition(mdp, s_from, s_to, wanted_action):
+    if s_from in mdp.terminal_states:
+        return 0
+    
+    possible_actions = [action for action in actions_to_idx if mdp.step(s_from, action) == s_to]
+    
+    prob = 0
+    for action in possible_actions:
+        prob += mdp.transition_function[wanted_action][actions_to_idx[action]]
+    return prob
+
+def total_utility(mdp, U, state, action):
+    utilities = [transition(mdp, state, s_to, action)*get_utility(U, s_to) for s_to in get_states(mdp)]
+    return sum(utilities)
+
+def get_utility(U, state):
+    i, j = state
+    return U[i][j]
+
+def utility(mdp, U, state, action):
+    actions = ["UP", "DOWN", "RIGHT", "LEFT"]
+    profits = [U[i_][j_] for i_, j_ in [mdp.step(state, a) for a in actions]]
+    probs = mdp.transition_function[action]
+    muls = [profit * prob for profit, prob in zip(profits, probs)]
+    return sum(muls)
 
 def get_legal_states(mdp):
     for i in range(mdp.num_row):
@@ -31,13 +80,7 @@ def value_iteration(mdp, U_init, epsilon=10 ** (-3)):
         
         for i, j in get_legal_states(mdp):
             reward = float(mdp.board[i][j])
-            sums = []
-            profits = [U[i_][j_] for i_, j_ in [mdp.step((i,j), a) for a in mdp.actions.keys()]]
-            for action in mdp.actions.keys():
-                probs = mdp.transition_function[action]
-                muls = [profit * prob for profit, prob in zip(profits, probs)]
-                sums.append(sum(muls))
-            
+            sums = [utility(mdp, U, (i,j), action) for action in mdp.actions.keys()]
             U_[i][j] = reward + gamma * max(sums)
             
             if abs(U_[i][j] - U[i][j]) > delta:
@@ -58,14 +101,9 @@ def get_policy(mdp, U):
     policy = deepcopy(U)
     
     for i, j in get_legal_states(mdp):
-        sums = []
-        profits = [U[i_][j_] for i_, j_ in [mdp.step((i,j), a) for a in mdp.actions.keys()]]
-        for action in mdp.actions.keys():
-            probs = mdp.transition_function[action]
-            muls = [profit * prob for profit, prob in zip(profits, probs)]
-            sums.append(sum(muls))
+        sums = [utility(mdp, U, (i,j), action) for action in mdp.actions.keys()]
         best_index = sums.index(max(sums))
-        policy[i][j] = ["UP, "DOWN", "RIGHT", "LEFT][best_index]
+        policy[i][j] = ["UP", "DOWN", "RIGHT", "LEFT"][best_index]
     return policy
     # ========================
 
@@ -77,10 +115,25 @@ def policy_evaluation(mdp, policy):
     #
 
     # ====== YOUR CODE: ======
-    raise NotImplementedError
+    
+    rewards = np.array([reward(mdp, s) for s in get_states(mdp)])
+    
+    transitions = np.array([[transition(mdp, s_from, s_to, action_from_policy(policy, s_from)) 
+                             for s_to in get_states(mdp)] for s_from in get_states(mdp)])
+    
+    utility = np.linalg.inv(np.eye(len(rewards)) - mdp.gamma * transitions) @ rewards
+    
+    # Turn utility vector to a map
+    U = deepcopy(policy)
+    for s, u in zip(get_states(mdp), utility.tolist()):
+        i, j = s
+        U[i][j] = u
+    
+    return U
+    
     # ========================
 
-
+    
 def policy_iteration(mdp, policy_init):
     # TODO:
     # Given the mdp, and the initial policy - policy_init
@@ -89,5 +142,26 @@ def policy_iteration(mdp, policy_init):
     #
 
     # ====== YOUR CODE: ======
-    raise NotImplementedError
+    
+    policy = deepcopy(policy_init)
+    
+    changed = True
+    i=0
+    while changed:
+        # print(f"iteration {i}")
+        i += 1
+        U = policy_evaluation(mdp, policy)
+        # print("policy evaluation:")
+        # mdp.print_utility(U)
+        changed = False
+        
+        for s in get_states(mdp):
+            utilities = [total_utility(mdp, U, s, a) for a in actions_to_idx]
+            if max(utilities) > total_utility(mdp, U, s, action_from_policy(policy, s)):
+                # print('max no good')
+                i, j = s
+                policy[i][j] = idx_to_actions[utilities.index(max(utilities))]
+                changed = True
+    return policy
+
     # ========================
